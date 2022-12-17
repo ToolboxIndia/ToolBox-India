cc963879db4b25064e8285eb6cd8e4cf0234ccaa
Rohan <rohanraut2300@gmail.com>
2022-12-17 12:28:25 +0530

    Removed demp activity

--- app/src/main/java/com/example/toolboxandroid/For_Work.java
+++ app/src/main/java/com/example/toolboxandroid/For_Work.java
@@ -5,31 +5,42 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.text.BreakIterator;
 import java.util.HashMap;
 
-public class For_Work extends AppCompatActivity {
+public class For_Work extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Button btnApply;
-    EditText numInput,addressIn;
+    Spinner cat;
+    EditText numInput,addressIn,city;
     SharedPreferences sharedPreferences;
     String EmailId;
     private static final String SHARED_PREF_NAME="mypref";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_for_work);
 
         btnApply = findViewById(R.id.btnApply);
         addressIn = findViewById(R.id.addressIn);
         numInput = findViewById(R.id.numInput);
+        city = findViewById(R.id._City);
+        cat = findViewById(R.id.category);
+
+        Spinner spinner = findViewById(R.id.category);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.cat, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
         sharedPreferences = getSharedPreferences(SHARED_PREF_NAME,MODE_PRIVATE);
 
@@ -48,12 +59,25 @@ public class For_Work extends AppCompatActivity {
                 // must be same in both saving and retrieving the data
                 w.put("Email", EmailId);
                 w.put("Address",addressIn.getText().toString());
+                w.put("City", city.getText().toString());
+                w.put("Category",cat.toString());
                 w.put("Experience",numInput.getText().toString());
                 FirebaseDatabase.getInstance().getReference().child("Workers").push().setValue(w);
                 Toast.makeText(For_Work.this, "Succes", Toast.LENGTH_SHORT).show();
 
             }
         });
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int i, long l) {
+        String text = parent.getItemAtPosition(i).toString();
+        Toast.makeText(parent.getContext(),text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
